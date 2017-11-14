60dedae7b39c0486e7254088ff2a8dfbc61c555c
wuhuanju <wuhuanju@cmhi.chinamobile.com>
2017-11-14 10:59:55 +0800

    test

---
+++ config.rb
@@ -0,0 +1,14 @@
+http_path = ""
+css_dir = "src/css"
+sass_dir = "src/css"
+images_dir = "src/images"
+javascripts_dir = "src/js"
+fonts_dir = "src/fonts"
+
+cache = false
+# :expanded、:nested 、:compact、:compressed
+output_style = :expanded
+# relative path
+relative_assets = true
+# commnets
+line_comments = false
